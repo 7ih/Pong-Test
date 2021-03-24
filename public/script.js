bab0c38da0a667f658b0f66af32976629dabe9ad
7ih <75285668+7ih@users.noreply.github.com>
2021-03-24 10:19:27 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -303,14 +303,15 @@ sp.start = function() {
       else if (posX > canvas.width) posX = -paddleWidth;
 
       paddleX = posX;
-      console.log("cursor move")
     }
   }
   function getStartXTouch(e) {
-    if (!gameActive) gameActive = true;
+    if (!gameActive) {
+      gameActive = true;
+      requestAnimationFrame(draw);
+    }
     touchStartX = e.changedTouches[0].clientX/bounds.width*canvas.width;
     e.preventDefault();
-    console.log("touch")
   }
   function movePaddleTouch(e){
     if (gameActive) {
@@ -322,7 +323,6 @@ sp.start = function() {
       paddleX = posX;
     }
     e.preventDefault();
-    console.log("move")
   }
   function pointerLock() {
     if (!gameActive) {
@@ -330,11 +330,9 @@ sp.start = function() {
       requestAnimationFrame(draw);
     }
     if (document.pointerLockElement !== canvas) canvas.requestPointerLock();
-    console.log("pointer lock")
   }
   function pauseOnUnfocus() {
     if (document.pointerLockElement !== canvas) gameActive = false;
-    console.log("pause")
   }
 
   canvas.addEventListener('mousemove', movePaddleMouse);
